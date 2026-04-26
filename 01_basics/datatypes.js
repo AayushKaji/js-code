7fec469fa39cab5bc7c635fad5326bec7ca4dbdb
AayushKaji <aayushkaji80@gmail.com>
2026-04-26 12:09:19 +0000

    comit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+//alert(3+3) we are using nodesjs not browser so it will not work
+
+// code readablity should be good
+
+let name = "aayush"
+let age = 21
+let isLoggedIn = false
+
+//number=>2 to power 53
+//bigint
+//string = ""
+//boolean
+//null = standalone value
+//undefined
+//symbol = uniqueness
+
+//object
+
+console.log(typeof("aayush"));//return the type 
+
+
+
